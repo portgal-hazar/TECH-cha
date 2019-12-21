3ef45b95ac82b35feaf52cdc7accfe15777635a6
basketlovr399 <58037205+basketlovr399@users.noreply.github.com>
2019-12-21 17:04:36 +0900

    Update mission_7-1_kakunin.php

--- mission_7-1_kakunin.php
+++ mission_7-1_kakunin.php
@@ -10,7 +10,7 @@
 	<table border="1">
 	<tr>
 		<th bgcolor=b0c4de>予約番号</th>
-		<th><input type="text" name="registernum" value="<?php echo $_POST['number']; ?>">
+		<th><input type="text" name="registernumber" value="<?php echo $_POST['number']; ?>">
 	</tr>
 	<tr>
 		<th bgcolor=b0c4de>ご選択日時</th>
@@ -26,7 +26,7 @@
 	</tr>
 	<tr>
 		<th bgcolor=b0c4de>メールアドレス(半角)</th>
-		<th><input type="text" name="mail" value="<?php echo $mail; ?>" placeholder="メールアドレス">
+		<th><input type="text" name="mail" value="<?php echo $email; ?>" placeholder="メールアドレス">
 	</tr>
 	<tr>
 		<th bgcolor=b0c4de>性別</th>
@@ -60,11 +60,56 @@ $sql = "SELECT*FROM テーブル名 WHERE ?=:?"; //←かようさんが作っ�
 	$results=$stmt->fetchall();
 foreach($results as $row){
 	$name=$row[""];
-	$mail=$row[""];
+	$email=$row[""];
 }
 
 if(empty($_POST["yoyaku"])){
 	echo "空欄を埋めてください!!";
 }else{
+	$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+		$id=$_POST["registernumber"];
+		$sql="SELECT * FROM mission7 WHERE id=:id";
+		$send=$pdo->prepare($sql);
+		$send->bindParam(":id", $id, PDO::PARAM_INT);
+		$newmail=$send->fetchall();
+	foreach($newmail as $host){
+		$host_name=$host[""];
+		$host_mail=$host[""];
+	}
+		
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+require 'setting.php';
+
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+
+    $mail->isSMTP();  
+    $mail->SMTPAuth = true;
+    $mail->Host = MAIL_HOST;  
+    $mail->Username = MAIL_USERNAME;  
+    $mail->Password = MAIL_PASSWORD;  
+    $mail->SMTPSecure = MAIL_ENCRPT;  
+    $mail->Port = SMTP_PORT; 
+
+    // メール内容設定
+    $mail->CharSet = "UTF-8";
+    $mail->Encoding = "base64";
+    $mail->setFrom(MAIL_FROM,MAIL_FROM_NAME);
+    $mail->addAddress('$_POST["mail"]', '$_POST["name"]'); 
+    $mail->addAddress("$host_mail","$host_mail");
+    $mail->Subject = MAIL_SUBJECT; 
+    $mail->isHTML(true);    
+    $body = "以下の内容で予";
+
+    $mail->Body  = $body; // メール本文
+    // メール送信の実行
+    if(!$mail->send()) {
+    	echo 'メッセージは送られませんでした！';
+    	echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+    	echo 'メールをおくりました。確認してください!!';
+    }
+
 }
 ?>
